3bb7f6ea25ae63cfdd7eba55faf8ccba519bfa61
Abdus Sattar Bhuiyan <sattar.kuet@gmail.com>
2020-02-23 14:21:50 +0600

    first commit

---
+++ foreach.php
@@ -0,0 +1,10 @@
+<?php
+$array = array(1,2,3,4,5);
+$students = array('name'=>'Abdus Sattar Bhuiyan','role'=>10);
+
+foreach( $students as $key => $value)
+{
+    echo $key.' : '.$value.'<br/>';
+}
+
+?>
